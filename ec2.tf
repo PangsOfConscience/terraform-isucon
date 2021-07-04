8bb99a4189c37f1a3bd503795701e85968aa3945
Kenji Shimizu <jrywm121@gmail.com>
2021-07-04 09:19:35 +0900

    🐛 add http and https rule to sg

--- ec2.tf
+++ ec2.tf
@@ -21,6 +21,22 @@ resource "aws_security_group" "isucon_qualify_instance_sg" {
   }
 
   ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
     from_port        = 8000
     to_port          = 8000
     protocol         = "tcp"
