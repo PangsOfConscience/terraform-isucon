51b13a54d0b224d32c6474230159cf0c33830349
Kenji Shimizu <jrywm121@gmail.com>
2021-07-04 11:13:52 +0900

    ✨ add mysql ingress

--- ec2.tf
+++ ec2.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "isucon_qualify_instance_sg" {
   }
 
   ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
     from_port        = 8000
     to_port          = 8000
     protocol         = "tcp"
