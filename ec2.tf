2fa849d9cd415beea2910752771a0a1977ff9fcf
Kenji Shimizu <jrywm121@gmail.com>
2021-07-18 13:35:46 +0900

    🐛 add 9091 port ingress to sg

--- ec2.tf
+++ ec2.tf
@@ -52,6 +52,13 @@ resource "aws_security_group" "isucon_qualify_instance_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    from_port        = 9091
+    to_port          = 9091
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
